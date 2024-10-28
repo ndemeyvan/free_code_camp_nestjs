4e0ac6000bfdf862783e59ce5d4b5e60047e1f03
ndemeyvanneero <yvan.ndeme@neero.io>
2024-10-29 00:33:05 +0100

    work with middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
+// implementer NestModele afin d'override configure et appeler notre middle ware
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes("songs");
+    // consumer.apply(LoggerMiddleware).forRoutes("songs");
+    // consumer.apply(LoggerMiddleware).forRoutes({path:"songs",method:RequestMethod.POST});// uniquement sur les requet post
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); // specifier directement le controller 
   }
 }
